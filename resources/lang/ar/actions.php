3dd798dcdb4aa156565f2a686af5a53e124cd642
zidane <zidane.ibnmajah@gmail.com>
2021-06-28 18:59:08 +0100

    oh nothing

--- resources/lang/ar/actions.php
+++ resources/lang/ar/actions.php
@@ -1,23 +1,23 @@
 <?php
 return [
 
-           ################################  index
-'add' => 'إضافة حدث',
-'nom_action' => ' اسم الحدث',
-'action' => 'العمليات',
-'title edit' => 'تعديل',
-'title supprimer' => 'حدف',
-'modal supprimer' => "حدف الحدث",
-'modal validation supprision' => 'هل أنت متأكد من عملية الإزالة ؟',
-'modal validation close' => 'الغاء',
-'modal validation confirm' => 'تاكيد',
-           ################################  create et edit  l'action
+    ################################  
+    'add' => 'إضافة حدث',
+    'nom_action' => ' اسم الحدث',
+    'action' => 'العمليات',
+    'title edit' => 'تعديل',
+    'title supprimer' => 'حدف',
+    'modal supprimer' => "حدف الحدث",
+    'modal validation supprision' => 'هل أنت متأكد من عملية الإزالة ؟',
+    'modal validation close' => 'الغاء',
+    'modal validation confirm' => 'تاكيد',
+    ################################  
     'nom_action_fr' => '   اسم الحدث بالفرنسية',
     'nom_action_ar' => ' اسم الحدث بالعربية',
     'btn_add_edit' => 'حفظ',
     'form.title' => 'يرجي ادخال اسم الحدث',
 
-    ################################  create  l'action -------- messages request
+    ################################  
 
     'action_fr required' => 'اسم  الحدث بالفرنسية إلزامي',
     'action_fr max' => 'يجب ألا تتجاوز الحدث  بالفرنسية 255 حرفًا',
@@ -28,11 +28,11 @@ return [
     'action_ar unique' => 'تم تسجيل الحدث بالعربية مسبقا',
     'action success in add' => 'تمت إضافة الحدث بنجاح',
 
-    ################################   edit  l'action
+    ################################  
     'edit action' => 'تعديل الحدث',
     'action success in edit' => 'تم تعديل الحدث بنجاح',
 
-    ################################   supprimer  l'action
+    ################################   
     'action success in supprimer' => 'تم حذف الحدث بنجاح',
 
 
--- resources/lang/fr/axes.php
+++ resources/lang/fr/axes.php
@@ -3,30 +3,22 @@ return [
     ################################  index
     "add" => "Ajouter un axe",
     "nom" => "Axe",
-
     "modal supprimer" => "Supprimer l'axe ",
-
     ################################  create et edit axe
     "champ_fr" => "Nom de l'axe",
     "champ_ar" => "اسم المحور ",
     "form.title" => "Veuillez saisir le nom d'Axe ",
-
-    ################################  create axe -------- messages request
-
+    ################################  Request Validation
     "axe_fr required" => "Le champ axe en francais est obligatoire",
     "axe_fr max" => "L'axe en francais  ne doit pas dépasser 250 caractères",
     "axe_fr unique" => "L'axe en francais  a déjà été enregistré",
-
     "axe_ar required" => "Le champ axe en arabe est obligatoire",
     "axe_ar max" => "L'axe en arabe ne doit pas dépasser 250 caractères",
     "axe_ar unique" => "L'axe en arabe a déjà été enregistré",
-
-    "axe success in add" => "L'axe a été ajouté avec succès",
+    "success_add" => "L'axe a été ajouté avec succès",
     ################################   edit axe
-
     "edit" => "Editer l'axe",
-    "axe success in edit" => "L'axe a été editer avec succès",
+    "success_edit" => "L'axe a été editer avec succès",
     ################################   supprimer axe
-    "axe success in supprimer" => "L'axe a été supprimé avec succès",
-
+    "success_supprimer" => "L'axe a été supprimé avec succès",
 ];
