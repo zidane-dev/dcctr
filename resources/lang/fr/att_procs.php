5090cace5e956504274c0a9b07f6414d44a5864e
zidane <zidane.ibnmajah@gmail.com>
2021-07-20 12:50:41 +0100

    go

---
+++ resources/lang/fr/att_procs.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    ################################  index
+    'attproc'           =>  'Attributions de processus',
+    'anneeobj'          =>  'Année d\'objectif',
+    'anneerls'          =>  'Année de réalisation',
+    'attribution'       =>  'Attribution',
+    'niveau'            =>  'Niveau',
+    'statut'            =>  'Statut',
+    'transferts'        =>  'Transferts',
+    'delegations'       =>  'Délégations',
+    'vos_attributions'  =>  'Nombre d\'attributions transférées à votre domaine : ',
+    'vos_delegations'   =>  'Nombre d\'attributions déléguées à votre service : ',
+    ################################  validation
+    'add'               =>  'Nouvelle Attribution',
+
+];
